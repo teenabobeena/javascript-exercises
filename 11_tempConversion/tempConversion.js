3e24a71b71d58b5d673cba7978d05ac0602d5826
teenabobeena <christinas.codingemail@gmail.com>
2025-05-12 01:35:33 -0400

    Solve 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const decimal = 5 / 9;
+  let firstStep = f - 32;
+
+  let resultC = firstStep * decimal;
+  return parseFloat(resultC.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let resultF = c * 1.8 + 32;
+
+  return parseFloat(resultF.toFixed(1));
 };
 
 // Do not edit below this line
