59f015341b82484a5100d10da853587d46e07b13
teenabobeena <christinas.codingemail@gmail.com>
2025-05-12 00:57:56 -0400

    Solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    } else if 
+        (typeof a !== 'number' || typeof b !== 'number') {
+            return 'ERROR'
+    } else if (a % 1 !== 0 || b % 1 !== 0) {
+        return 'ERROR'
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        } 
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
